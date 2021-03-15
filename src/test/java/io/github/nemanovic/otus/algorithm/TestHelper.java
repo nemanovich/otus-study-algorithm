35cacc728a55f80f6174e6df33725ba1c21a18dc
nemanovic <nemanovich@gmail.com>
2021-03-15 19:21:27 +0300

    [hw3] add bitboard solution

--- src/test/java/io/github/nemanovic/otus/algorithm/TestHelper.java
+++ src/test/java/io/github/nemanovic/otus/algorithm/TestHelper.java
@@ -1,8 +1,24 @@
 package io.github.nemanovic.otus.algorithm;
 
+import io.github.nemanovic.otus.algorithm.hw3.BitboardSolutionTest;
 import org.junit.jupiter.params.ParameterizedTest;
+
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
 
 public class TestHelper {
     public static final String METHOD_NAME = ParameterizedTest.DISPLAY_NAME_PLACEHOLDER +
             ParameterizedTest.DEFAULT_DISPLAY_NAME;
+
+    public static Object[] readResource(String path, final Function<Scanner, Object[]> readFunction) {
+        try (Scanner s = new Scanner(BitboardSolutionTest.class.getResourceAsStream(path))) {
+            return readFunction.apply(s);
+        }
+    }
+
+    public static Object[] concat(Object[] a, Object[] b) {
+        return Stream.of(a, b).flatMap(Stream::of).toArray();
+    }
 }
